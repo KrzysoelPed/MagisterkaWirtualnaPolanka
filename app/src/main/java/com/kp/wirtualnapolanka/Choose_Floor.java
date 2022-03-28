bf27511761980e174ddfab41e4969e098e1b259d
Marcin Siuda <marcin-siuda@o2.pl>
2022-03-29 00:09:37 +0200

    Dodanie parteru

--- app/src/main/java/com/kp/wirtualnapolanka/Choose_Floor.java
+++ app/src/main/java/com/kp/wirtualnapolanka/Choose_Floor.java
@@ -1,6 +1,10 @@
 package com.kp.wirtualnapolanka;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
 import androidx.appcompat.widget.Toolbar;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,6 +12,9 @@ import androidx.appcompat.app.AppCompatActivity;
 public class Choose_Floor extends AppCompatActivity {
 
     Toolbar toolbar;
+    ImageButton button_ground;
+    Intent button_ground_intent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate (savedInstanceState);
@@ -15,6 +22,18 @@ public class Choose_Floor extends AppCompatActivity {
 
         toolbar = findViewById (R.id.toolbar);
         setSupportActionBar (toolbar);
+
+        button_ground = findViewById(R.id.ground);
+
+        button_ground.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                button_ground_intent = new Intent(Choose_Floor.this, Ground_floor.class);
+                startActivity(button_ground_intent);
+            }
+        });
+
+
 
     }
 }
