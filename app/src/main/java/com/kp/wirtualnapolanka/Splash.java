642896e50f41a4a16713140fe19e96fc2b66a2fb
KrzysoelPed <krzysztof@retia.pl>
2022-03-28 16:28:11 +0200

    Panel Logowania i Rejestracji

---
+++ app/src/main/java/com/kp/wirtualnapolanka/Splash.java
@@ -0,0 +1,15 @@
+package com.kp.wirtualnapolanka;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate (savedInstanceState);
+        setContentView (R.layout.splash_activity);
+
+
+}}
